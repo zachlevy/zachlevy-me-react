45afcc6d8a09b1aa613295d466de091a101c8d70
Zachary Levy <zacharyalevy@gmail.com>
2017-05-07 10:44:48 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <Navbar />
+        <div className="blog-header">
+          <div className="container">
+            <h1 className="blog-title">The Bootstrap Blog</h1>
+            <p className="lead blog-description">An example blog template built with Bootstrap.</p>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="blog-masthead">
+        <div className="container">
+          <nav className="nav blog-nav">
+            <a className="nav-link active" href="#">Home</a>
+            <a className="nav-link" href="#">Side Projects</a>
+            <a className="nav-link" href="#">First Exit Media</a>
+            <a className="nav-link" href="#">Contact</a>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
