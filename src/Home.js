e7f9295e60fef554008f34ee835a9d9ebceb1d98
Zachary Levy <zacharyalevy@gmail.com>
2017-05-07 19:33:02 -0400

    skills section

--- src/Home.js
+++ src/Home.js
@@ -14,6 +14,36 @@ class Home extends Component {
             </div>
           </div>
         </div>
+        <div className="row">
+          <div className="col-12 col-sm-4">
+            <h3>Favorite Stack</h3>
+            <ul>
+              <li>Ruby on Rails</li>
+              <li>React</li>
+            </ul>
+            <p>Other than that, anything that solves the problem</p>
+          </div>
+          <div className="col-12 col-sm-4">
+            <h3>Other Tools</h3>
+            <ul>
+              <li>Photoshop</li>
+              <li>Illustrator</li>
+              <li>Trello</li>
+            </ul>
+          </div>
+          <div className="col-12 col-sm-4">
+            <h3>Self-Deprecated Stack</h3>
+            <p>Hopefully I'll never have to use these again</p>
+            <ul>
+              <li>PHP / Drupal</li>
+              <li>Python* / Django</li>
+              <li>ES5 / CoffeeScript / Angular1</li>
+              <li>Objective-C</li>
+              <li>Java</li>
+            </ul>
+            <p>*Alright, Python is still pretty awesome</p>
+          </div>
+        </div>
       </div>
     );
   }
