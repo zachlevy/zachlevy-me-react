02ce78efcd9aa2cc1118f01c7139b7fe1e0d485a
Zachary Levy <zacharyalevy@gmail.com>
2017-05-07 22:55:38 -0400

    bugfix youtube link

--- src/Footer.js
+++ src/Footer.js
@@ -16,7 +16,7 @@ class Footer extends Component {
                 <li className="list-inline-item"><h4><a href="https://www.facebook.com/zacharyaaronlevy" target="_blank" ><i className="fa fa-facebook"></i></a></h4></li>
                 <li className="list-inline-item"><h4><a href="https://twitter.com/zachary_levy" target="_blank" ><i className="fa fa-twitter"></i></a></h4></li>
                 <li className="list-inline-item"><h4><a href="https://www.linkedin.com/in/zacharylevy/" target="_blank" ><i className="fa fa-linkedin"></i></a></h4></li>
-                <li className="list-inline-item"><h4><a href="https://www.youtube.com/channel/UC6g3w6cVXLuPvrRjBOmQtag" target="_blank" ><i className="fa fa-youtube"></i></a></h4></li>
+                <li className="list-inline-item"><h4><a href="https://www.youtube.com/channel/UC8TCWJJKgsFJFHM-pVlTSEg" target="_blank" ><i className="fa fa-youtube"></i></a></h4></li>
               </ul>
             </div>
           </div>
