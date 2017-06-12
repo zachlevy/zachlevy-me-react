00b574003ba3730f9b89a185cda4ee78aeff5fa1
Zachary Levy <zacharyalevy@gmail.com>
2017-06-12 13:57:31 -0400

    pwepare to die

--- src/Blog/PhotoshopBattles/battles.js
+++ src/Blog/PhotoshopBattles/battles.js
@@ -11,5 +11,10 @@ export const battles = [
     originalImageUrl: "https://placehold.it/1024x768?text=Original",
     uploadUrl: "example2_1024x768.png",
     uploadTitle: "Snarky Title"
+  }, {
+    originalUrl: "https://www.reddit.com/r/photoshopbattles/comments/6grwyf/psbattle_little_girl_with_a_water_gun/", // reddit url
+    originalImageUrl: "https://i.imgur.com/KvMphmi.png", // absolute url to original image pre-photoshop
+    uploadUrl: "pwepare_to_die_20170612.jpg", // filename of uploaded image in public/images/psbattles/ directory
+    uploadTitle: "Pwepare to Die" // fun snarky title used as comment on reddit
   }
 ]
