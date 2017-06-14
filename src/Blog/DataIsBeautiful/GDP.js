9668fbda74fb459a448fdaefd62ecc5ee7d4808d
Zachary Levy <zacharyalevy@gmail.com>
2017-06-13 22:38:14 -0400

    tooltip, cleanup

--- src/Blog/DataIsBeautiful/GDP.js
+++ src/Blog/DataIsBeautiful/GDP.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import { Helmet } from "react-helmet"
 
-import { ResponsiveContainer, PieChart, Pie, Legend } from 'recharts'
+import { Tooltip, ResponsiveContainer, PieChart, Pie, Legend } from 'recharts'
+
+import './foreignAid.css'
 
 // import data from './foreignAidGdp.json'
 import foreignAidPercentageOfGdp from './foreignAidPercentageOfGdp.json'
@@ -13,6 +15,22 @@ class GDP extends Component {
 
   render() {
     console.log("render")
+
+    const tooltip = (tooltipObject) => {
+      if (tooltipObject.active) {
+        const content = tooltipObject.content
+        const {name, value} = tooltipObject.payload[0]
+        const {country, dollars, fill} = tooltipObject.payload[0].payload
+        return (
+          <div className="foreignAidPercentageOfGdp-tooltip">
+            <div className="country"><span>{country}</span></div>
+            <div className="dollars"><span>${dollars} Billion USD</span></div>
+          </div>
+        )
+      }
+
+    }
+
     return (
       <div>
         <Helmet>
@@ -21,27 +39,34 @@ class GDP extends Component {
         </Helmet>
         <div className="row">
           <div className="col-12">
-            <h1>ok</h1>
+            <h1>Foreign Aid By Country</h1>
             <ResponsiveContainer width="100%" height={500}>
               <PieChart>
-                <Pie dataKey="dollars" startAngle={180} endAngle={0} data={foreignAidByCountry} cx={"50%"} cy={"75%"} outerRadius={300} fill="#000000" label/>
+                <Tooltip content={tooltip}/>
+                <Pie dataKey="dollars" startAngle={180} endAngle={0} data={foreignAidByCountry} cx={"50%"} cy={"75%"} outerRadius={300} fill="#ffffff" />
               </PieChart>
             </ResponsiveContainer>
-            <svg>
-              <defs>
-                {
-                  countries.map((country, index) => {
-                    const width = "700"
-                    const height = "500"
-                    return (
-                      <pattern key={index} id={`${country}-flag`} patternUnits="userSpaceOnUse" width={width} height={height}>
-                        <image xlinkHref={`/images/countriesFlagsSvgs/${country}.svg`} x="0" y="0" width={width} height={height}></image>
-                      </pattern>
-                    )
-                  })
-                }
-              </defs>
-            </svg>
+            <div>
+              <svg>
+                <defs>
+                  {
+                    countries.map((country, index) => {
+                      const width = "700"
+                      const height = "500"
+                      return (
+                        <pattern key={index} id={`${country}-flag`} patternUnits="userSpaceOnUse" width={width} height={height}>
+                          <image xlinkHref={`/images/countriesFlagsSvgs/${country}.svg`} x="0" y="0" width={width} height={height}></image>
+                        </pattern>
+                      )
+                    })
+                  }
+                </defs>
+              </svg>
+            </div>
+            <div>
+              <h4>Sources</h4>
+              <p><a href="https://en.wikipedia.org/wiki/List_of_development_aid_country_donors#Net_official_development_assistance_by_country_as_a_percentage_of_gross_national_income_in_2015" target="_blank">https://en.wikipedia.org/wiki/List_of_development_aid_country_donors</a></p>
+            </div>
           </div>
         </div>
       </div>
@@ -53,7 +78,7 @@ export default GDP
 
 
 
-// https://en.wikipedia.org/wiki/List_of_development_aid_country_donors#Net_official_development_assistance_by_country_as_a_percentage_of_gross_national_income_in_2015
+//
 // Country, %
 // Sweden, 1.40
 // Norway, 1.05
