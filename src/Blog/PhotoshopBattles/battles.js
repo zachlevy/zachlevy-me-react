e2c0b77a1b9de485652f1da4f0fae584ec9420ce
Zachary Levy <zacharyalevy@gmail.com>
2017-07-22 12:22:32 -0400

    jaguar leopard psbattle

--- src/Blog/PhotoshopBattles/battles.js
+++ src/Blog/PhotoshopBattles/battles.js
@@ -11,5 +11,10 @@ export const battles = [
     originalImageUrl: "https://i.imgur.com/KvMphmi.png", // absolute url to original image pre-photoshop
     uploadUrl: "pwepare_to_die_20170612.jpg", // filename of uploaded image in public/images/psbattles/ directory
     uploadTitle: "Pwepare to Die" // fun snarky title used as comment on reddit
+  }, {
+    originalUrl: "https://www.reddit.com/r/photoshopbattles/comments/6ov4fa/psbattle_this_leopard_with_a_fish/",
+    originalImageUrl: "https://i.redditmedia.com/tqLRgyxfrBhIu611ailqBFJa2Pc2njTCXkozDsgcM-A.jpg?w=1024&s=fbabde9586c8b6b4097d0a424494cd21",
+    uploadUrl: "jaguar_20170722.jpg",
+    uploadTitle: "Driving a Leopard"
   }
 ]
