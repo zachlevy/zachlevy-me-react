57a8e6a4ef721fed7e4d9f910efc23e1cf279c7f
Zachary Levy <zacharyalevy@gmail.com>
2017-11-11 13:35:47 -0500

    added ridiculous history

--- src/Products/All.js
+++ src/Products/All.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import lifeProTipsPromoImg from '../images/life_pro_tips_book_promo_1200x628.jpg'
 import historyMappedLogo from '../images/history_mapped_logo_1200x628.png'
 import physlyLogo from '../images/physly_logo_1200x628.png'
+import ridiculousHistoryLogo from '../images/ridiculous_history_podcast_1200x628.jpg'
 
 
 class All extends Component {
@@ -45,6 +46,22 @@ class All extends Component {
         </div>
         <hr />
         <div className="row">
+          <div className="col-12 col-sm-6">
+            <div>
+              <a href="http://ridiculoushistory.com" target="_blank" >
+                <img src={ridiculousHistoryLogo} className="img-fluid" alt="Ridiculous History Podcast" />
+              </a>
+            </div>
+            <br />
+            <h4>Ridiculous History</h4>
+            <h5>Podcast</h5>
+            <p>We're two guys that love history and the absurdity of things people did that are often glossed over. We've had many outrageous discussions, so we thought why not record them and put it on the internet.</p>
+            <ul>
+              <li><Link to="http://ridiculoushistory.com" target="_blank">Listen on ridiculoushistory.com</Link></li>
+              <li><Link to="https://itunes.apple.com/ca/podcast/ridiculous-history/id1311950885" target="_blank">Listen on iTunes</Link></li>
+              <li><Link to="https://play.google.com/music/podcasts/portal/#p:id=playpodcast/series&a=1045165628" target="_blank">Listen on Google Play</Link></li>
+            </ul>
+          </div>
           <div className="col-12 col-sm-6">
             <div>
               <a href="https://phys.ly" target="_blank" >
